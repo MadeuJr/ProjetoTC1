d783af1f9373b340147dbe01d00e42cd68595963
Amadeu Andrade Jr <amadeu.junior@aluno.ifsp.edu.br>
2022-12-03 22:29:18 -0300

    Validação email

--- ProjetoPT2/scripts/seguradoras.js
+++ ProjetoPT2/scripts/seguradoras.js
@@ -1,78 +1,43 @@
 function setFormAddSeguradora() {
     let xml =
-        '<form class="pure-form border">' +
-        "<fieldset>" +
-        '<h3 class="is-center">Cadastro de novo veículo</h3>' +
-        "<hr>" +
+        '<form onsubmit="if (ValidateEmail() == false) {return false} " class="pure-form border">' +
+        '<fieldset>' +
+        '<h3 class="is-center">Cadastro de nova Seguradora</h3>' +
+        '<hr>' +
         '<div class="pure-g marginForm">' +
-        '<div class="pure-u-1-12 l-box-form">' +
-        '<label for="placaL">Letras</label>' +
-        '<input class="pure-input-rounded" name="placaL" type="text" placeholder="ABC" maxlength="3" minlength="3" pattern="[A-Z]*" required />' +
-        "</div>" +
-        '<div class="pure-u-2-24 l-box-form">' +
-        '<label for= "placaN" > Numeros</label >' +
-        '<input class="pure-input-rounded" name="placaN" type="text" inputmode="numeric" placeholder="1234" maxlength="4" minlength="4" pattern="d*" required />' +
-        " </div >" +
-        ' <div class="pure-u-3-24 l-box-form">' +
-        '<label for="placaEstado">Estado</label>' +
-        '<input class="pure-input-rounded" name="placaEstado" type="text" placeholder="Estado" minlength="2" maxlength="2" pattern="[A-Z]*" required />' +
-        "</div>" +
-        ' <div class="pure-u-3-24 l-box-form">' +
-        '<label for="placaCidade">Cidade</label>' +
-        '<input class="pure-input-rounded" name="placaCidade" type="text" placeholder="Cidade" required />' +
-        "</div>" +
+
+        '<div class="pure-u-2-5 l-box-form">' +
+        '<label for="segCNPJ">CNPJ</label>' +
+        '<input class="pure-input-rounded" name="placaL" type="text" placeholder="CNPJ" required />' +
+        '</div>' +
+        '<div class="pure-u-2-5 l-box-form">' +
+        '<label for="segRazaoSoc">Razão Social</label>' +
+        '<input class="pure-input-rounded" name="placaN" placeholder="Razão Social" type="text" required  />' +
+        '</div>' +
+
         '<div class="pure-u-3-24 l-box-form">' +
-        "<br>" +
-        '<label for="vTipo">Tipo:</label>' +
-        '<select class="pure-input-rounded" name="tipo" required>' +
-        " <option selected disabled></option>" +
-        '<option value="Carro">Carro</option>' +
-        '<option value="Moto">Moto</option>' +
-        "</select>" +
-        "</div>" +
+        '<label for="segInitDate">Data de inicio</label>' +
+        '<input class="pure-input-rounded" name="placaEstado" type="date" required />' +
+        ' </div>' +
+
+        ' <div class="pure-u-2-5 l-box-form">' +
+        '<label for="segEmail">E-mail</label>' +
+        '<input class="pure-input-rounded" name="segEmail" id="segEmail" type="text" placeholder="example@example.com" required />' +
+        '<span class="result" id="result"></span>' +
+        '</div>' +
+
         '<div class="pure-u-3-24 l-box-form">' +
-        '<label for="vMarca">Marca do veículo</label>' +
-        '<input class="pure-input-rounded" name="vMarca" type="text" placeholder="Insira uma Marca" required />' +
-        "</div>" +
-        ' <div class="pure-u-3-24 l-box-form">' +
-        '<label for="vModelo">Modelo do veículo</label>' +
-        '<input class="pure-input-rounded" name="vModelo" type="text" placeholder="Insira um modelo" required />' +
-        "</div>" +
-        "<div>" +
-        ' <label for="vAno">Ano do veículo</label>' +
-        '<input class="pure-input-rounded" name="vAno" type="text" inputmode="numeric" placeholder="1980-2023" maxlength="4" minlength="4" pattern="d*" required />' +
-        "</div>" +
-        '<div class="pure-u-3-6 l-box-form">' +
-        "<br>" +
-        '<label for="combustivel">Combutivel do veículo:</label>' +
-        '<select class="pure-input-rounded" name="combustivel" required>' +
-        "<option selected disabled></option>" +
-        '<option value="Gasolina">Gasolina</option>' +
-        '<option value="Alcool">Alcool</option>' +
-        '<option value="Flex">Flex</option>' +
-        '<option value="Diesel">Diesel</option>' +
-        "</select>" +
-        "</div>" +
-        "</div >" +
-        '<div class="container">' +
-        '<ul class="boxs-container is-center">' +
-        '<label for="acessorios">Acessorios</label>' +
-        "<br>" +
-        '<li><input type="checkbox" id="checkbox-1" value="molasEsportivas"><label for="checkbox-1">Molas esportivas</label></li>' +
-        '<li><input type="checkbox" id="checkbox-2" value="farolLed"><label for="checkbox-2">Farol Led</label></li>' +
-        '<li><input type="checkbox" id="checkbox-3" value="palhetas"><label for="checkbox-3">Palhetas</label></li>' +
-        '<li><input type="checkbox" id="checkbox-4" value="GPS"><label for="checkbox-4">GPS</label></li>' +
-        '<li><input type="checkbox" id="checkbox-5" value="centralMultimidia"><label for="checkbox-5">Central Multimídia</label></li>' +
-        '<li><input type="checkbox" id="checkbox-6" value="Alarme"><label for="checkbox-6">Alarme</label></li>' +
-        '<li><input type="checkbox" id="checkbox-7" value="airbag"><label for="checkbox-7">Airbag</label></li>' +
-        '<li><input type="checkbox" id="checkbox-8" value="suporteCelular"><label for="checkbox-8">Suporte para Celular</label></li>' +
-        "</ul>" +
-        " </div>" +
-        ' <div class="is-center">' +
-        '<button type="submit" class="button">Cadastrar</button>' +
-        " </div>" +
-        "</fieldset>" +
-        "</form>";
+        '<label for="segTel">Telefone</label>' +
+        '<input class="pure-input-rounded" name="segTel" type="tel" inputmode="numeric" placeholder="1611223344" maxlength="11" minlength="10" pattern="\\d*" required/>' +
+        '</div> ' +
+        '<div class="pure-u-1 is-center">' +
+        '<button class="button">Cadastrar</button>' +
+        '</div>' +
+
+        '</div>' +
+
+        '</fieldset>' +
+        '</form>';
 
     document.getElementById("forms").innerHTML = xml;
 }
@@ -81,11 +46,11 @@ function setFormDeleteSeguradora() {
     xml =
         '<form class="pure-form border">' +
         "<fieldset>" +
-        '<h3 class="is-center">Exclusão de veículo</h3>' +
+        '<h3 class="is-center">Exclusão de Seguradora</h3>' +
         "<hr>" +
         '<div class="marginForm is-center">' +
         "<br>" +
-        '<label for="idVeículo">ID do veículo</label>' +
+        '<label for="idSeg">ID da Seguradora</label>' +
         '<select class="pure-input-rounded" name="combustivel" required>' +
         "<option selected disabled></option>" +
         '<option value="1">1</option>' +
@@ -93,6 +58,7 @@ function setFormDeleteSeguradora() {
         '<option value="3">3</option>' +
         '<option value="4">4</option>' +
         "</select>" +
+        "<br>" +
         "<br>" +
         '<label for="confirmationExclude">Confirma exclusão?</label>' +
         '<input name="confirmationExclude" type="checkbox" required>' +
@@ -108,94 +74,81 @@ function setFormDeleteSeguradora() {
 
 function setFormUpdateSeguradora() {
     xml =
-        '<form class="pure-form border">' +
-        "<fieldset>" +
-        '<h3 class="is-center">Alteração de veículo</h3>' +
-        "<hr>" +
-        '<div class="marginForm is-center">' +
-        "<br>" +
-        '<label for="idVeículo">ID do veículo</label>' +
-        '<select class="pure-input-rounded" name="combustivel" required>' +
-        "<option selected disabled></option>" +
-        '<option value="1">1</option>' +
-        '<option value="2">2</option>' +
-        '<option value="3">3</option>' +
-        '<option value="4">4</option>' +
-        "</select>" +
-        "<br>" +
-        "</div>" +
+        '<form onsubmit="if (ValidateEmail() == false) {return false} " class="pure-form border">' +
+        '<fieldset>' +
+        ' <h3 class="is-center">Alteração de Seguradora</h3>' +
+        ' <hr>' +
+        ' <div class="marginForm is-center">' +
+        ' <br>' +
+        ' <label for="idVeículo">ID da Seguradora</label>' +
+        ' <select class="pure-input-rounded" name="combustivel" required>' +
+        ' <option selected disabled></option>' +
+        ' <option value="1">1</option>' +
+        ' <option value="2">2</option>' +
+        ' <option value="3">3</option>' +
+        ' <option value="4">4</option>' +
+        ' </select>' +
+        '<br>' +
+        ' <br>' +
+        '</div>' +
         '<div class="pure-g marginForm">' +
-        '<div class="pure-u-1-12 l-box-form">' +
-        '<label for="placaL">Letras</label>' +
-        '<input class="pure-input-rounded" name="placaL" type="text" placeholder="ABC" maxlength="3" minlength="3" pattern="[A-Z]*" required />' +
-        "</div>" +
-        '<div class="pure-u-2-24 l-box-form">' +
-        '<label for= "placaN" > Numeros</label >' +
-        '<input class="pure-input-rounded" name="placaN" type="text" inputmode="numeric" placeholder="1234" maxlength="4" minlength="4" pattern="d*" required />' +
-        " </div >" +
+
+        '<div class="pure-u-2-5 l-box-form">' +
+        '<label for="segCNPJ">CNPJ</label>' +
+        '<input class="pure-input-rounded" name="placaL" type="text" placeholder="CNPJ" required />' +
+        '</div>' +
+        ' <div class="pure-u-2-5 l-box-form">' +
+        ' <label for="segRazaoSoc">Razão Social</label>' +
+        '<input class="pure-input-rounded" name="placaN" placeholder="Razão Social" type="text"required />' +
+        '</div>' +
+
         ' <div class="pure-u-3-24 l-box-form">' +
-        '<label for="placaEstado">Estado</label>' +
-        '<input class="pure-input-rounded" name="placaEstado" type="text" placeholder="Estado" minlength="2" maxlength="2" pattern="[A-Z]*" required />' +
-        "</div>" +
+        '<label for="segInitDate">Data de inicio</label>' +
+        '<input class="pure-input-rounded" name="placaEstado" type="date" required />' +
+        '</div>' +
+
+        '<div class="pure-u-2-5 l-box-form">' +
+        ' <label for="segEmail">E-mail</label>' +
+        '<input class="pure-input-rounded" name="segEmail" id="segEmail" type="text"placeholder="example@example.com" required />' +
+        '<span class="result" id="result"></span>' +
+        '</div>' +
+
         ' <div class="pure-u-3-24 l-box-form">' +
-        '<label for="placaCidade">Cidade</label>' +
-        '<input class="pure-input-rounded" name="placaCidade" type="text" placeholder="Cidade" required />' +
-        "</div>" +
-        '<div class="pure-u-3-24 l-box-form">' +
-        "<br>" +
-        '<label for="vTipo">Tipo:</label>' +
-        '<select class="pure-input-rounded" name="tipo" required>' +
-        " <option selected disabled></option>" +
-        '<option value="Carro">Carro</option>' +
-        '<option value="Moto">Moto</option>' +
-        "</select>" +
-        "</div>" +
-        '<div class="pure-u-3-24 l-box-form">' +
-        '<label for="vMarca">Marca do veículo</label>' +
-        '<input class="pure-input-rounded" name="vMarca" type="text" placeholder="Insira uma Marca" required />' +
-        "</div>" +
-        ' <div class="pure-u-3-24 l-box-form">' +
-        '<label for="vModelo">Modelo do veículo</label>' +
-        '<input class="pure-input-rounded" name="vModelo" type="text" placeholder="Insira um modelo" required />' +
-        "</div>" +
-        "<div>" +
-        ' <label for="vAno">Ano do veículo</label>' +
-        '<input class="pure-input-rounded" name="vAno" type="text" inputmode="numeric" placeholder="1980-2023" maxlength="4" minlength="4" pattern="d*" required />' +
-        "</div>" +
-        '<div class="pure-u-3-6 l-box-form">' +
-        "<br>" +
-        '<label for="combustivel">Combutivel do veículo:</label>' +
-        '<select class="pure-input-rounded" name="combustivel" required>' +
-        "<option selected disabled></option>" +
-        '<option value="Gasolina">Gasolina</option>' +
-        '<option value="Alcool">Alcool</option>' +
-        '<option value="Flex">Flex</option>' +
-        '<option value="Diesel">Diesel</option>' +
-        "</select>" +
-        "</div>" +
-        "</div >" +
-        '<div class="container">' +
-        '<ul class="boxs-container is-center">' +
-        '<label for="acessorios">Acessorios</label>' +
-        "<br>" +
-        '<li><input type="checkbox" id="checkbox-1" value="molasEsportivas"><label for="checkbox-1">Molas esportivas</label></li>' +
-        '<li><input type="checkbox" id="checkbox-2" value="farolLed"><label for="checkbox-2">Farol Led</label></li>' +
-        '<li><input type="checkbox" id="checkbox-3" value="palhetas"><label for="checkbox-3">Palhetas</label></li>' +
-        '<li><input type="checkbox" id="checkbox-4" value="GPS"><label for="checkbox-4">GPS</label></li>' +
-        '<li><input type="checkbox" id="checkbox-5" value="centralMultimidia"><label for="checkbox-5">Central Multimídia</label></li>' +
-        '<li><input type="checkbox" id="checkbox-6" value="Alarme"><label for="checkbox-6">Alarme</label></li>' +
-        '<li><input type="checkbox" id="checkbox-7" value="airbag"><label for="checkbox-7">Airbag</label></li>' +
-        '<li><input type="checkbox" id="checkbox-8" value="suporteCelular"><label for="checkbox-8">Suporte para Celular</label></li>' +
-        "</ul>" +
-        " </div>" +
-        ' <div class="is-center">' +
-        '<button type="submit" class="button">Cadastrar</button>' +
-        " </div>" +
-        "</fieldset>" +
-        "</form>" +
-        "</fieldset>" +
-        "</form>";
+        '<label for="segTel">Telefone</label>' +
+        '<input class="pure-input-rounded" name="segTel" type="tel" inputmode="numeric" placeholder="1611223344" maxlength="11" minlength="10" pattern="\d*" required />' +
+        '</div>' +
+        '<div class="pure-u-1 is-center">' +
+        '<button class="button">Cadastrar</button>' +
+        '</div>' +
+
+        '</div>' +
+
+        '</fieldset>' +
+        '</form>';
     document.getElementById("forms").innerHTML = xml;
 }
 
 
+function ValidateEmail() {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var input = document.getElementById("segEmail")
+    if (input.value.match(mailformat)) {
+        document.getElementById("result").innerHTML = ""
+
+        return true;
+    }
+    else {
+
+        document.getElementById("result").innerHTML = "E-mail inválido, insira um e-mail válido"
+        return false;
+    }
+}
+
+function submitVerify() {
+    if (ValidateEmail() == true) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
